7896f700b73c83a4c091b5da7399f3fe8cbb977e
LiangHuang15 <17520728@gm.uit.edu.vn>
2021-08-25 15:40:17 +0700

    single products

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,23 @@
         }
         document.body.classList.toggle('dark-theme')
      })
+     $('.container-blur').click(function(){
+         console.log('Chọn')
+     })
+
+
+
+     var temp=$('.pro-top__review__list li img')
+     document.getElementById('pro-top__review__large-img').style.backgroundImage ="url('"+temp[0].src+"')"
+
+
+     var liWidth= parseInt($('.pro-top__review__item')[0].offsetWidth)
+    $('.pro-top__review__item').height(liWidth);
+    function reportWindowSize(){
+        var liWidth= parseInt($('.pro-top__review__item')[0].offsetWidth)
+    $('.pro-top__review__item').height(liWidth);
+    }
+    window.onresize = reportWindowSize;
 });
 // Hide search bar
 document.getElementById('icon-hide-search').onclick=function(){
@@ -31,20 +48,42 @@ document.getElementById('header-search-icon').onclick= function (){
         document.getElementById('search__input').focus();
     }
 }
- $(document).ready(function(){
-    $('').click(function(){
-
-    });
- })
-
 
 document.getElementById('header-nav__menu-icon').onclick= function(){
     var item=document.getElementById('header-nav-list');
     var container= document.getElementById('container');
-    console.log(container)
-    item.classList.toggle('header-nav-list-active');   
-    container.classList.toggle('container-blur') 
+     
+    if(item.classList.toggle('header-nav-list-active'))  {
+        $('.header-menu-icon').removeClass('fa-bars').addClass('fa-times')
+    }else{
+        $('.header-menu-icon').removeClass('fa-times').addClass('fa-bars')
+    }
+    container.classList.toggle('container-blur')
 }
 
+// Single-products
+$('.pro-top__review__list li img').click(function() {
+    $('.pro-top__review__item--active').removeClass('pro-top__review__item--active')
+    $(this).parent().addClass('pro-top__review__item--active')
+    var src=$(this).attr('src');
+    document.getElementById('pro-top__review__large-img').style.backgroundImage ="url('"+src+"')"
+});
 
+$('#pro-bottom__viewtype-btn').click(function(){
+    $('.pro-bottom__details--desc').toggleClass('pro-bottom__details--desc-see-less');
+    if( $('.pro-bottom__details--desc').hasClass('pro-bottom__details--desc-see-less')){
+        $('#pro-bottom__viewtype-btn').text('See less')
+    }else{
+        $('#pro-bottom__viewtype-btn').text('View more')
+    }
+    
+})
+$(document).ready(function(){
+    console.log($('.pro-bottom__details--desc').height())
+    if($('.pro-bottom__details--desc').height() <400)
+    {
+    $('#pro-bottom__viewtype-btn').hide()
+    }
+
+})
 
